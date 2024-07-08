4a88a146c4015ecf9e60dcf5d29970783fa93c4f
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-08 16:49:54 -0600

    Created Cart Icon and Cart Dropdown

--- live-clothing/src/routes/navigation/navigation.component.jsx
+++ live-clothing/src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import { UserContext } from "../../contexts/user.context";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 import { signOutUser } from "../../utils/firebase";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -27,7 +29,9 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+          <CartIcon></CartIcon>
         </div>
+        <CartDropdown></CartDropdown>
       </div>
       <Outlet />
     </Fragment>
