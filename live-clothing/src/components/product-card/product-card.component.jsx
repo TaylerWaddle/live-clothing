c7d245800ff2ee24c9d5a93e6236ead5a466f76c
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-10 13:13:56 -0600

    Added CartItem, ProductCard, and addItemToCart function

--- live-clothing/src/components/product-card/product-card.component.jsx
+++ live-clothing/src/components/product-card/product-card.component.jsx
@@ -1,17 +1,24 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
 import "./product-card.styles.scss";
 
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
-  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
-      <img src={imageUrl} alt={`${name}`} />
+      <img src={product.imageUrl} alt={`${product.name}`} />
       <div className="footer">
-        <span className="name">{name}</span>
-        <span className="price">${price}</span>
+        <span className="name">{product.name}</span>
+        <span className="price">${product.price}</span>
       </div>
-      <Button buttonType="inverted">ADD TO CART</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        ADD TO CART
+      </Button>
     </div>
   );
 };
