047ed3e6d632ab374bc46c4e9d3d229c517fd4c9
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-11 02:49:27 -0600

    Created Navigation to Checkout Page

--- live-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ live-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,28 @@
-import "./cart-dropdown.styles.scss";
-import Button from "../button/button.component";
-import CartItem from "../cart-item/cart-item.component";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
+import { useNavigate } from "react-router-dom";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+
+import "./cart-dropdown.styles.scss";
+
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckout = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items"></div>
       {cartItems.map((item) => (
         <CartItem key={item.id} cartItem={item}></CartItem>
       ))}
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckout}>CHECKOUT</Button>
     </div>
   );
 };
