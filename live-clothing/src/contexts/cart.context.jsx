e0bf9ab03039546d8385ce09cad220a1ac94a11b
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-11 03:29:26 -0600

    Added Checkout Page cartItem decrement function

--- live-clothing/src/contexts/cart.context.jsx
+++ live-clothing/src/contexts/cart.context.jsx
@@ -18,11 +18,29 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  // check if quantity === 1, if so, reomve item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  // if not, return back cartItems with matching cart item with reduce quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
   cartCount: 0,
 });
 
@@ -43,10 +61,15 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemFromCart,
     cartItems,
     cartCount,
   };
--- live-clothing/src/routes/checkout/checkout.component.jsx
+++ live-clothing/src/routes/checkout/checkout.component.jsx
@@ -4,7 +4,8 @@ import { useContext } from "react";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart } = useContext(CartContext);
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
 
   return (
     <div>
@@ -16,7 +17,9 @@ const Checkout = () => {
             <div key={id}>
               <h2>{name}</h2>
               <span>{quantity}</span>
-              <span>decrement</span>
+              <span onClick={() => removeItemFromCart(cartItem)}>
+                decrement
+              </span>
               <span onClick={() => addItemToCart(cartItem)}>increment</span>
             </div>
           );
