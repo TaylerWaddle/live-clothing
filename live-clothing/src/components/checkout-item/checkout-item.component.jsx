ac8284bea780f8f719725592ddc6e9ce3a44785e
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-11 04:46:53 -0600

    Finished checkout-item component

--- live-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ live-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -4,8 +4,13 @@ import { CartContext } from "../../contexts/cart.context";
 import "./checkout-item.styles.scss";
 
 const CheckoutItem = ({ cartItem }) => {
-  const { clearItemFromCheckout } = useContext(CartContext);
+  const { clearItemFromCheckout, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
   const { name, imageUrl, price, quantity } = cartItem;
+
+  const clearItemHandler = () => clearItemFromCheckout(cartItem);
+  const incrementItemHandler = () => addItemToCart(cartItem);
+  const decrementItemHandler = () => removeItemFromCart(cartItem);
 
   return (
     <div className="checkout-item-container">
@@ -13,12 +18,17 @@ const CheckoutItem = ({ cartItem }) => {
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className="name"> {name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={decrementItemHandler}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={incrementItemHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div
-        className="remove-button"
-        onClick={() => clearItemFromCheckout(cartItem)}
-      >
+      <div className="remove-button" onClick={clearItemHandler}>
         &#10005;
       </div>
     </div>
